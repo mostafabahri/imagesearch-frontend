7d165e8ef271dfa8559f3fb245b36c7648413a62
Mostafa Bahri <mostafabahri@git.commit>
2020-10-19 08:59:29 +0330

    purge tailwind for prod

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,16 @@
-const autoprefixer = require('autoprefixer');
-const tailwindcss = require('tailwindcss');
-
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./public/**/*.html', './src/**/*.vue'],
+  defaultExtractor: (content) => {
+    return content.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
+  },
+})
 module.exports = {
   plugins: [
-    tailwindcss,
-    autoprefixer,
+    require('autoprefixer'),
+    require('tailwindcss'),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
+
   ],
 };
