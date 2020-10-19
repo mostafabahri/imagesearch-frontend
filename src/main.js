98df1265f1b2ba56636dc91b3f8d40f3c4ba791b
Mostafa Bahri <mostafabahri@git.commit>
2020-10-19 09:01:17 +0330

    Dont need a store

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
-import store from './store';
 import VueProgressBar from 'vue-progressbar';
 import VueLazyload from 'vue-lazyload';
 
@@ -33,6 +32,5 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
   render: h => h(App),
 }).$mount('#app');
